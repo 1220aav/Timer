d6ffae2859a590b031b9837631cd4e7b2271ada4
Andrey <1220aav@gmail.com>
2020-03-12 21:32:03 +0000

    simple stopwatch implementation

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var timerOutlet: UILabel!
+    
+    var counter = 0.0
+    var timer = Timer()
+    var isCounterON = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        timerOutlet.text = String(counter)
     }
-
-
+    
+    // MARK: - Actions
+    
+    @IBAction func switchTimer(_ sender: UIButton) {
+        if !isCounterON {
+            startTimer()
+            sender.setTitle("Pause", for: .normal)
+            sender.tintColor = UIColor.systemRed
+        } else {
+            stopTimer()
+            sender.setTitle("Play", for: .normal)
+            sender.tintColor = UIColor.systemBlue
+        }
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        isCounterON = true
+    }
+    
+    private func stopTimer() {
+        timer.invalidate()
+        isCounterON = false
+    }
+    
+    @objc private func updateTimer() {
+        counter += 0.1
+        timerOutlet.text = String(format: "%.1f", counter)
+    }
 }
-
